b693cca9b096c9240bea7824bd327020a0746209
acostacion <carmengb27@ucm.es>
2026-04-03 19:14:03 +0200

    estableciendo muy por encima la base

--- Alcachofita_2/Assets/Scripts/Telemetria/EventTracker.cs
+++ Alcachofita_2/Assets/Scripts/Telemetria/EventTracker.cs
@@ -1,43 +1,60 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using static UnityEditor.ShaderData;
 using UnityEngine.SocialPlatforms;
+using static UnityEditor.ShaderData;
 
-public class EventTracker : MonoBehaviour
-{
-    static private EventTracker _instance;
-    public static EventTracker Instance { get { return _instance; } }
-
+public class EventTracker : MonoBehaviour {
+    static private EventTracker _instance = null;
+    static public EventTracker Instance { get { return _instance; } }
     void Awake()
     {
-        if (_instance == null)
-        {
+        if (_instance == null) {
             _instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else
-        {
+        else {
             Destroy(gameObject);
             return;
         }
     }
 
-        /// <summary>
-        /// • Centralización del punto de entrada del sistema de telemetría en un objeto accesible desde cualquier punto de nuestro juego.
-        /// • Puede requerir de una inicialización y finalización explícitas.
-        /// • En la inicialización se pueden enviar eventos de inicio de sesión junto con parámetros que pueden aportar especificaciones adicionales: plataforma, SO, país,
-        /// información demográfica (año de nacimiento, sexo, id de alguna red social. . . ).
-        /// </summary>
+    /// <summary>
+    /// • Centralización del punto de entrada del sistema de telemetría en un objeto accesible desde cualquier punto de nuestro juego.
+    /// • Puede requerir de una inicialización y finalización explícitas.
+    /// • En la inicialización se pueden enviar eventos de inicio de sesión junto con parámetros que pueden aportar especificaciones adicionales: plataforma, SO, país,
+    /// información demográfica (año de nacimiento, sexo, id de alguna red social. . . ).
+    /// </summary>
+    
+    // Ej.: JSON, CSV. XML, YAML, Binary Formats
+    enum SerializationType { JSON }
+    SerializationType _serializationType;
 
-    void Start()
-    {
-        
-    }
+    // Ej.: Disco, Servidor, Dispositivo que se este usando
+    enum PersistenceType { File }
+    PersistenceType _persistenceType;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        _persistenceType = PersistenceType.File; // por ejemplo
+        _serializationType = SerializationType.JSON; // por ejemplo
+
+        // 1. primero vemos de que formato seran los cosas
+        switch (_persistenceType) {
+            case PersistenceType.File:
+                switch (_serializationType) {
+                    case SerializationType.JSON:
+                        // TODO crear un objeto persistencia file de json.
+                    break;
+                        // ... TODO: aniadir otros casos si los hacemos...
+                }
+            break;
+                // ... TODO: aniadir otros casos si los hacemos...
+        }
+
+        // 2. colas de eventos
+        // TODO
+
+        // 3...
+
     }
 }
